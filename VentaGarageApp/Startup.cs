7afc420c96ad3883a6adec2f938627ba287ac8a9
EdsonAlcala <edson_alcala@msn.com>
2015-10-25 17:00:01 -0500

    first commit

---
+++ VentaGarageApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VentaGarageApp.Startup))]
+namespace VentaGarageApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
